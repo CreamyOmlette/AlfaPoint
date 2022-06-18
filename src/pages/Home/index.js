9171f61321a023f662d9af5adf3599fe71109a60
Alex Lipceanu <alexlipceanu@Alexs-MacBook-Pro-6.local>
2022-06-18 20:05:01 +0300

    new block: who we are

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,22 @@ export function Home() {
     <>
       <HeroSlider></HeroSlider>
       <IconSlider></IconSlider>
+      <div className="who-we-are">
+        <div className="content">
+          <div className="content-box">
+            <h2>Who are we?</h2>
+            <p>
+              Alfapoint is a modern and ambitious IT company available
+              worldwide, headquartered in Chisinau, the Republic of Moldova, a
+              country well-known for cost-effective offshore and nearshore
+              solutions. Based on our experience within the EU market and highly
+              skillful and dedicated team of experts, we deliver personalized
+              solutions to our customers prior to their needs.
+            </p>
+          </div>
+        </div>
+        <div className="who-we-are-bg">smth</div>
+      </div>
     </>
   );
 }
