18d4945233d84e8c7f3e5115d716c29be3827934
Alex Lipceanu <alexlipceanu@Alexs-MacBook-Pro-6.local>
2022-06-28 17:47:17 +0300

    added primitive emailing

--- website/src/components/contact-form/index.js
+++ website/src/components/contact-form/index.js
@@ -1,24 +1,69 @@
 import "./style.scss";
+import { useEffect, useState } from "react";
 
 export function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [subject, setSubject] = useState("");
+  useEffect(() => {
+    setSubject(
+      `subject:${formState.subject};name:${formState.name},email: ${formState.email}`
+    );
+  }, [formState]);
+
   return (
     <div className="contact-section">
       <div className="form">
         <h2>We’re here for you</h2>
         <p>Let’s discuss your needs as soon as possible</p>
         <form>
-          <input type={"text"} name="name" placeholder="Name, Surname" />
-          <input type={"text"} name="subject" placeholder="Subject" />
-          <input type={"text"} name="email" placeholder="Email" />
+          <input
+            value={formState.name}
+            onChange={(e) =>
+              setFormState({ ...formState, name: e.target.value })
+            }
+            type={"text"}
+            name="name"
+            placeholder="Name, Surname"
+          />
+          <input
+            value={formState.subject}
+            onChange={(e) =>
+              setFormState({ ...formState, subject: e.target.value })
+            }
+            type={"text"}
+            name="subject"
+            placeholder="Subject"
+          />
+          <input
+            type={"text"}
+            name="email"
+            placeholder="Email"
+            value={formState.email}
+            onChange={(e) =>
+              setFormState({ ...formState, email: e.target.value })
+            }
+          />
           <textarea
             name="description"
             cols="40"
             rows="5"
             placeholder="Message"
+            value={formState.message}
+            onChange={(e) =>
+              setFormState({ ...formState, message: e.target.value })
+            }
           ></textarea>
           <div className="btn-container">
-            <a className="submit" href="/#">
-              Send
+            <a
+              className="submit"
+              href={`mailto: d.lipceanu@alfa-point.com?subject=${subject}&body=${formState.message}`}
+            >
+              Send Email
             </a>
           </div>
         </form>
